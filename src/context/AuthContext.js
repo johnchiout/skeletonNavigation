d12ac5e20ada35e208e096980b854503e2a1afab
giannischiout <giannis.chiout@gmail.com>
2023-04-06 10:59:19 +0300

    logout action

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -12,15 +12,15 @@ export const AuthLayout = ({children}) => {
   const authContext = React.useMemo(() => {
     return {
       signIn: () => {
-        console.log('works')
+        console.log('sign in')
         setIsLoading(false);
         setIsLogged(true);
       },
       signUp: () => {
         setIsLoading(false);
-        setUserToken("asdf");
       },
       signOut: () => {
+        console.log('sign out')
         setIsLoading(false);
         setIsLogged(false);
       }
--- src/navigation/index.jsx
+++ src/navigation/index.jsx
@@ -1,7 +1,8 @@
 import { useState, useContext } from "react";
+import {Text, View} from 'react-native'
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createDrawerNavigator,  DrawerContentScrollView, DrawerItem, DrawerItemList, } from "@react-navigation/drawer";
 import { AuthContext } from "../context/AuthContext";
 
 import { ProfileScreen, HomeScreen, LoginScreen } from "../screens";
@@ -12,6 +13,7 @@ const AuthStack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
 
+//Login Screen - Stack:
 const AuthStackScreen = () => (
   <AuthStack.Navigator>
     <AuthStack.Screen
@@ -24,22 +26,36 @@ const AuthStackScreen = () => (
 );
 
 
-
+//Other Main Screens - Drawer:
 const DrawerScreen = () => (
-  <Drawer.Navigator initialRouteName="Profile">
+  <Drawer.Navigator initialRouteName="Profile" drawerContent={(props) => <CustomDrawerContent {...props} />}>
     <Drawer.Screen name="Home" component={HomeScreen} options={{ title: "Home" }}/>
     <Drawer.Screen name="Profile" component={ProfileScreen} options={{ title: "Profile" }}/>
   </Drawer.Navigator>
 );
 
+const CustomDrawerContent = (props) => {
+  const {authContext} = useContext(AuthContext)
+  
+  return (
+    <View>
+       <DrawerItemList {...props} />
+      <DrawerItem
+        label="Logout"
+        onPress={authContext.signOut}
+      />
+    </View>
+  )
+}
 
 
 const RootStack = createStackNavigator();
 
 
+//Combine Drawer and Stack:
 const RootStackScreen = () => {
+
     const {isLogged} = useContext(AuthContext);
-    console.log(isLogged)
     return (
       <RootStack.Navigator 
         screenOptions={{headerShown: false}}
